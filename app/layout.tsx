0c403dadd1256c5638d4644cd371da531c7bd534
Philip Köhler <p.koehler@neusta.de>
2025-08-09 19:44:44 +0200

    update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,25 @@
-import "./globals.css"
-import type { Metadata } from "next"
+import "./globals.css";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Philip Cinematic Universe",
   description: "Eine sehr persönliche Website - gemacht mit Herz und AI",
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="de">
-      <head>    <meta
+      <head>
+        <meta
           name="google-site-verification"
           content="0ofPNoFjh1HXsmLtm7gz0El0joyDZrrisyYAHTCjdAM"
-        /></head>
+        />
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
